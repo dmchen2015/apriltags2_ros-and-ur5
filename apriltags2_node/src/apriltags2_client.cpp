1610cbdb8f6f06cf4bd58e4e1eeb9fd897db3b77
harrycomeon <15265323451@163.com>
2019-05-18 20:54:38 +0800

    Add files via upload

---
+++ apriltags2_node/src/apriltags2_client.cpp
@@ -0,0 +1,52 @@
+#include <ros/ros.h>
+#include <apriltags2_node/LocalizePart.h>
+
+class ScanNPlan
+{
+public:
+  ScanNPlan(ros::NodeHandle& nh)
+  {
+    vision_client_ = nh.serviceClient<apriltags2_node::LocalizePart>("localize_part");
+  }
+
+  void start(const std::string& base_frame)
+  {
+    ROS_INFO("Attempting to localize part");
+    // Localize the part
+    apriltags2_node::LocalizePart srv;
+
+    srv.request.base_frame = base_frame;
+    ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+
+    if (!vision_client_.call(srv))
+    {
+      ROS_ERROR("Could not localize part");
+      return;
+    }
+    ROS_INFO_STREAM("part localized: " << srv.response);
+    //ROS_INFO_STREAM("frame_id: " << srv.request.base_frame);
+  }
+
+private:
+  // Planning components
+  ros::ServiceClient vision_client_;
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "apriltags2_node");
+  ros::NodeHandle nh;
+  ros::NodeHandle private_node_handle ("~");
+
+  ROS_INFO("ScanNPlan node has been initialized");
+  std::string base_frame;
+  private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
+
+  ScanNPlan app(nh);
+  ros::Duration(.5).sleep();  // wait for the class to initialize
+  app.start(base_frame);
+
+  
+
+  ros::spin();
+}
