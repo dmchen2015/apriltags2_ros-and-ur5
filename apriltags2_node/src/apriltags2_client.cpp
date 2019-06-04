61b04f9e3bad9407cdab22aee9e562cdac0d69d0
harrycomeon <15265323451@163.com>
2019-06-04 19:16:13 +0800

    Add files via upload

--- apriltags2_node/src/apriltags2_client.cpp
+++ apriltags2_node/src/apriltags2_client.cpp
@@ -4,12 +4,18 @@
 
 #include <moveit/move_group_interface/move_group_interface.h>
 
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <apriltags2_node/PlanCartesianPath.h>
+
+
 class ScanNPlan
 {
 public:
-  ScanNPlan(ros::NodeHandle& nh)
+  ScanNPlan(ros::NodeHandle& nh) : ac_("arm_controller/follow_joint_trajectory", true)//joint_trajectory_action
   {
     vision_client_ = nh.serviceClient<apriltags2_node::LocalizePart>("localize_part");
+    cartesian_client_ = nh.serviceClient<apriltags2_node::PlanCartesianPath>("plan_path");
   }
 
   void start(const std::string& base_frame)
@@ -20,30 +26,64 @@ public:
 
     srv.request.base_frame = base_frame;
     ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
-
-    if (!vision_client_.call(srv))
+    //如果服务回调不成功
+    if(!vision_client_.call(srv))
     {
       ROS_ERROR("Could not localize part");
-      return;
+      return ;
     }
+
     ROS_INFO_STREAM("part localized: " << srv.response);
     //ROS_INFO_STREAM("frame_id: " << srv.request.base_frame);
     //LocalizePart服务的响应后来初始化一个新move_target变量
-    geometry_msgs::Pose move_target = srv.response.pose;
+    //ROS_INFO_STREAM("part localized: " << srv.response.pose.orientation);
+    //srv.response.pose.orientation.z=(-1)*srv.response.pose.orientation.z;
+    //srv.response.pose.orientation.y=(-1)*srv.response.pose.orientation.y;
+    //srv.response.pose.orientation.x=(-1)*srv.response.pose.orientation.x;
+    //ROS_INFO_STREAM("part localized: " << srv.response.pose.orientation);
+    //交换了y，z轴，实现了机械手朝下
+    move_target = srv.response.pose;
+    move_target1=srv.response.pose;
+    move_target.orientation.y=move_target1.orientation.z;
+    move_target.orientation.z=move_target1.orientation.y;
+ 
+    //ROS_INFO_STREAM("part localized: " << move_target.orientation);
     //创建一个对象，move_group（）构造函数定义计划组名称
     moveit::planning_interface::MoveGroupInterface move_group("manipulator");
 
     // Plan for robot to move to part
     //使用move_group对象的setPoseTarget功能设置所需的笛卡尔目标位置
     move_group.setPoseReferenceFrame(base_frame);
-    move_group.setPoseTarget(move_target);
+    move_group.setPoseTarget(move_target); 
     move_group.move();
-    
-  }
 
+
+    // Plan cartesian path
+    apriltags2_node::PlanCartesianPath cartesian_srv;
+    cartesian_srv.request.pose = move_target;
+    if (!cartesian_client_.call(cartesian_srv))
+    {
+      ROS_ERROR("Could not plan for path");
+      return;
+    }
+
+    // Execute descartes-planned path directly (bypassing MoveIt)
+    ROS_INFO("Got cart path, executing");
+    control_msgs::FollowJointTrajectoryGoal goal;
+    goal.trajectory = cartesian_srv.response.trajectory;
+    ac_.sendGoal(goal);
+    ac_.waitForResult();
+    ROS_INFO("Done");
+
+  }
+  
 private:
   // Planning components
   ros::ServiceClient vision_client_;
+  geometry_msgs::Pose move_target;
+  geometry_msgs::Pose move_target1;
+  ros::ServiceClient cartesian_client_;
+  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac_;
 };
 
 int main(int argc, char **argv)
@@ -56,15 +96,20 @@ int main(int argc, char **argv)
   ros::AsyncSpinner async_spinner(1);
   async_spinner.start();
 
+  while(ros::ok())
+  {
+    ROS_INFO("ScanNPlan node has been initialized");
+    std::string base_frame;
+    private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
 
-  ROS_INFO("ScanNPlan node has been initialized");
-  std::string base_frame;
-  private_node_handle.param<std::string>("base_frame", base_frame, "world"); // parameter name, string object reference, default value
-
-  ScanNPlan app(nh);
-  ros::Duration(.5).sleep();  // wait for the class to initialize
-  app.start(base_frame);
-
-  ros::waitForShutdown();
-  //ros::spin();
+    ScanNPlan app(nh);
+    ros::Duration(.5).sleep();  // wait for the class to initialize
+    app.start(base_frame);
+    ros::Duration(.1000).sleep();
+    ros::Duration(.1000).sleep();
+    //ros::waitForShutdown();
+    //ros::spinOnce();
+    ros::Duration(.5).sleep();
+  }
+  return 0;
 }
--- apriltags2_node/src/atnode_vision.cpp
+++ apriltags2_node/src/atnode_vision.cpp
@@ -1,17 +1,20 @@
 #include "ros/ros.h"
-#include "apriltags2_ros/AprilTagDetectionArray.h"
+#include "tf2_msgs/TFMessage.h"
 #include <apriltags2_node/LocalizePart.h>
 #include <iostream>
 
 #include <tf/transform_listener.h>
 
+//float t=-1;
 class Localizer
 {
 public:
   Localizer(ros::NodeHandle& nh)
   {
-      ar_sub_ = nh.subscribe<apriltags2_ros::AprilTagDetectionArray>("tag_detections", 1,
+       
+      ar_sub_ = nh.subscribe<tf2_msgs::TFMessage>("tf", 1,
       &Localizer::number_callback, this);
+      ROS_INFO("Vision node starting1");
       server_ = nh.advertiseService("localize_part", &Localizer::localizePart, this); //将服务公布给ROS主服务器
   }
 
@@ -19,9 +22,10 @@ public:
                   apriltags2_node::LocalizePart::Response& res)
   {
      // Read last message
-      apriltags2_ros::AprilTagDetectionArrayConstPtr p = last_msg_;  
+      tf2_msgs::TFMessageConstPtr p = last_msg_;  
       if (!p) 
         return false;
+      
 
       //res.pose = p->detections[0].pose.pose.pose;
       //ROS_INFO_STREAM("frame_id: " << p->detections[0].pose.header.frame_id);
@@ -29,7 +33,12 @@ public:
       //将线上格式geometry_msgs::Pose转换为：tf::Transform object
       //tf::Transform target_to_cam;
       tf::Transform cam_to_target;
-      tf::poseMsgToTF(p->detections[0].pose.pose.pose, cam_to_target);//可以换成target to cam
+      //geometry_msgs::Quaternion target1={-1,0,0,0,0,1,0,0,0,0,-1,0,0,0,0,1};
+      //p->detections[0].pose.pose.pose.orientation::x=t*p->detections[0].pose.pose.pose.orientation::x;
+      //p->detections[0].pose.pose.pose.orientation.x=t*p->detections[0].pose.pose.pose.orientation.x;
+      //p->detections[0].pose.pose.pose.orientation.z=t*p->detections[0].pose.pose.pose.orientation.z;
+      //tf::poseMsgToTF(p->transforms[0].transform, cam_to_target);//可以换成target to cam
+      tf::transformMsgToTF(p->transforms[0].transform, cam_to_target);
       //cam_to_target=target_to_cam;//可以换成target to cam
 
       //使用侦听器对象request.base_frame从ARMarker消息（应该是“camera_frame”）查找和参考帧之间的最新转换：lookupTransform（）可以获得两个坐标系的转换-旋转的平移
@@ -37,11 +46,12 @@ public:
     //  try{
      //      listener_.lookupTransform(req.base_frame, p->detections[0].pose.header.frame_id, ros::Time(0), req_to_cam);
      //  }
+      
         try{
         ros::Time now = ros::Time::now();
-        listener_.waitForTransform(req.base_frame, p->detections[0].pose.header.frame_id, 
+        listener_.waitForTransform(req.base_frame, p->transforms[0].header.frame_id, 
                               now, ros::Duration(1.0));
-        listener_.lookupTransform(req.base_frame, p->detections[0].pose.header.frame_id,
+        listener_.lookupTransform(req.base_frame, p->transforms[0].header.frame_id,
                              now, req_to_cam);
             }
        catch (tf::TransformException &ex) {
@@ -57,14 +67,15 @@ public:
 
       return true;
   }
-  void number_callback(const apriltags2_ros::AprilTagDetectionArray::ConstPtr& msg)
+  void number_callback(const tf2_msgs::TFMessage::ConstPtr& msg)
   {
-      last_msg_ = msg;
+      if(msg->transforms[0].header.frame_id=="camera")
+         last_msg_ = msg;
       //ROS_INFO_STREAM(last_msg_->detections[0].pose.pose.pose);
   }
 
   ros::Subscriber ar_sub_;
-  apriltags2_ros::AprilTagDetectionArrayConstPtr last_msg_;
+  tf2_msgs::TFMessageConstPtr last_msg_;
   ros::ServiceServer server_;
   //定义监听器
   tf::TransformListener listener_; 
@@ -73,6 +84,7 @@ public:
 
 int main(int argc, char **argv)
 {
+    
     ros::init(argc, argv,"apriltag_detector_subscriber");
     ros::NodeHandle node_obj;
     Localizer localizer(node_obj);
